000084e2532b9e56a77dca90cc62384fb36c8d6e
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-22 16:48:09 -0300

    c2-adding picture and several modify

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require('express')
 const app = express()
+
+
+app.get('/test',(req, res) => {
+   res.send({ name:'test here now!!'})
+})
+
+
+
+
+
+// another way for listin of door!!
+const port = process.env.PORT || 3000
+app.listen((port), err => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log('we are always')
+    }
+})
