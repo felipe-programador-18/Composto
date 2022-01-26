2987f18689b05e2a003c0780964ac2bf792977f7
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 15:07:57 -0300

    c46- seeing and modify code

---
+++ router/client.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const composto = require('../lib/composto')
+
+router.get('/composto', (req,res) => {
+    // desctructment assiment
+    const {C, i, t} = req.query
+
+    if(C && i && t){
+        const teste = CalcJuros.CalcJuros(C,i,t)
+        res.render('composto', {
+            error: false,
+            C : composto.Convert(C),
+            i: composto.Convert(i),
+            t: composto.Convert(t),
+            teste: composto.Convert(teste)
+        })
+    }else {
+        res.render('composto', {
+        error: 'Valores inválidos, preencha Todos os Campos!!!'
+        })
+    }
+  
+}) 
+
+module.exports = router
