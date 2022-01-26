c831af565e55748f1c3f2f49bdae8ff89848068a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 14:35:04 -0300

    c39- create file of index to use router

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const composto = require('./lib/composto')
+//const composto = require('./lib/composto')
 
 //The command path serve to caught html and css
 const path = require('path')
-
-
 
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -13,41 +11,8 @@ app.set('views', path.join(__dirname, 'views'))
 //this is for use in the css!!!
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-app.get('/',(req, res) => {
-   res.render('main', {
-
-   })
-})
-
-
-app.get('/composto', (req,res) => {
-    // desctructment assiment
-    const {C, i, t} = req.query
-
-    if(C && i && t){
-        const teste = CalcJuros.CalcJuros(C,i,t)
-        res.render('composto', {
-            error: false,
-            C : composto.Convert(C),
-            i: composto.Convert(i),
-            t: composto.Convert(t),
-            teste: composto.Convert(teste)
-        })
-    }else {
-        res.render('composto', {
-        error: 'Valores inválidos, preencha Todos os Campos!!!'
-        })
-    }
-  
-})
-
-
-
-
-
-
+const Indexrouter = require('./router/index')
+app.use('./', Indexrouter)
 
 
 // another way for listin of door!!
