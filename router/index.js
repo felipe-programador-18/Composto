1edf15f753ef2a015b78f50dd6e85e9497c6ae39
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 15:07:28 -0300

    c45- create more a router to gordura

--- router/index.js
+++ router/index.js
@@ -1,32 +1,11 @@
 //reorrange this code in file with router
 const express = require('express')
 const router = express.Router()
-const composto = require('../lib/composto')
 
 router.get('/',(req, res) => {
     res.render('main', {
     })
  })
 
- router.get('/composto', (req,res) => {
-    // desctructment assiment
-    const {C, i, t} = req.query
-
-    if(C && i && t){
-        const teste = CalcJuros.CalcJuros(C,i,t)
-        res.render('composto', {
-            error: false,
-            C : composto.Convert(C),
-            i: composto.Convert(i),
-            t: composto.Convert(t),
-            teste: composto.Convert(teste)
-        })
-    }else {
-        res.render('composto', {
-        error: 'Valores inválidos, preencha Todos os Campos!!!'
-        })
-    }
-  
-}) 
 
 module.express= router
